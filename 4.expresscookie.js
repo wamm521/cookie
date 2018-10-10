7fcbf42e6c00e8f5b189e37bc096384542f6c628
sunhuijuan <673599808@qq.com>
2018-10-10 19:27:13 +0800

    提交文件

--- 4.expresscookie.js
+++ 4.expresscookie.js
@@ -10,11 +10,11 @@ app.get('/',function(req,res){
     //req.cookies指的就是用户提交过来的cookie对象
     var isVisited=req.cookies.isVisited;
     if(isVisited){
-        res.send('欢迎老朋友');
+        res.send('欢迎老朋友1');
     }else{
         //res.cookie这个方法其实是没有的，这个是cookieParsesr里面带的
         res.cookie('isVisited','1',{maxAge:20*1000});
-        res.send('欢迎新朋友');
+        res.send('欢迎新朋友1');
     }
 })
 app.listen(8080);
