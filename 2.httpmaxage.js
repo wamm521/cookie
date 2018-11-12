eb85f8d3da78954924df94e16fe696475a7d28d7
sunhuijuan <673599808@qq.com>
2018-11-12 14:54:03 +0800

    提交文件333

--- 2.httpmaxage.js
+++ 2.httpmaxage.js
@@ -8,14 +8,14 @@ http.createServer(function(req,res){
     if(pathname=='/write'){
         var ts=new Date(Date.now()+20*1000).toGMTString();
         // console.log(Date.now());
-        res.setHeader('Set-Cookie','name=zfpx2; Expires='+ts);
+        res.setHeader('Set-Cookie','name=zfpx2; maxAge='+ts);
         res.end('write');
     }else if(pathname=='/read'){
         //读取请求头中的cookie
         var cookie=req.headers.cookie;
         var cookieObj=querystring.parse(cookie,';');
         console.log(cookieObj);
-        res.end('read');
+        res.end('read222');
     }
 
 }).listen(8080);
